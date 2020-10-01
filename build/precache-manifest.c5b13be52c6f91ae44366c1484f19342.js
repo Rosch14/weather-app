0cd76ab37a16771e435889a06c9d3718f3e5089f
roschnzi@gmail.com <roschnzi@gmail.com>
2020-10-01 03:16:51 +0000

    weather app

---
+++ build/precache-manifest.c5b13be52c6f91ae44366c1484f19342.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4020a16aeaae70dbd4f0b69acfd02b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c98b774b9b99192606",
+    "url": "/static/css/main.5823ff1d.chunk.css"
+  },
+  {
+    "revision": "a51e54d97bcd5b566e2d",
+    "url": "/static/js/2.1e13b469.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1e13b469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c98b774b9b99192606",
+    "url": "/static/js/main.2f3362c1.chunk.js"
+  },
+  {
+    "revision": "4993598d539d3cedf09f",
+    "url": "/static/js/runtime-main.a15d9368.js"
+  },
+  {
+    "revision": "02c1e9bf3ba4c20c769149de7bd1a14f",
+    "url": "/static/media/22.02c1e9bf.jpg"
+  }
+]);
